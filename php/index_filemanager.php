045d67e919b4aae50fb3cc4c4d82e0b343b7479b
joshfiel <joshfiel>
2004-09-15 19:05:51 +0000

    initial mFile splash page

---
+++ php/index_filemanager.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link href="fileman.css" rel="stylesheet" type="text/css" />
+<title>Welcome to mFile (beta)</title>
+</head>
+<body>
+<div id="color-headbar-blue">
+    <?php include( 'display/includes/todaysdate.php' ); ?>
+    <h1>mFile: AFS File Management</h1>
+</div>
+<div id="itcs"><a href="http://www.itcs.umich.edu/"><b>information technology central
+            services </b>at the university of michigan</a> </div>
+<div id="menubar">
+    <ul>
+        <li><a href="./dropoff">Drop Off</a></li>
+        <li><a href="./publish">Publish</a></li>
+        <li><a href="./share">Share</a></li>
+        <li><a href="./organize">Organize</a></li>
+        <li><a href="./permissions">Permissions</a></li>
+        <li><a href="https://weblogin.umich.edu/cgi-bin/logout?http://afs.umich.edu/">Logout</a></li>
+    </ul>
+</div>
+<div id="welcome" class="serviceBox">
+    <h2>Welcome to mFile</h2>
+    <p>mFile is a web-based interface to the AFS network storage system. Use mFile to
+        store, access, and share documents on any computer with a web browser. This is
+        a beta (test) version of mFile. If you encounter any problems or have suggestions
+        for improvement, please let us know!</p>
+</div>
+<div id="dropoff" class="serviceBox">
+    <h2>Dropoff</h2>
+    <p>- Create &quot;dropboxes&quot; that others can use to submit files (such as assignments).</p>
+    <p>- View dropboxes that others have created for you to use.</p>
+</div>
+<div id="publish" class="serviceBox">
+    <h2>Publish</h2>
+    <p>Publish html documents on the Internet. You can create public web sites that everyone
+        can view or private sites that only certain people can view.</p>
+</div>
+<div id="share" class="serviceBox">
+    <h2>Share</h2>
+    <p>Create special folders that contain documents you wish to share with other people.</p>
+</div>
+<div id="organize" class="serviceBox">
+    <h2>Organize</h2>
+    <p>Upload, download, move, delete, rename, and view the files in your AFS space.</p>
+</div>
+<div id="permissions" class="serviceBox">
+    <h2>Permissions</h2>
+    <p>Users familiar with AFS file permissions can set access controls for folders in
+        their AFS spaces.</p>
+</div>
+</body>
+</html>
