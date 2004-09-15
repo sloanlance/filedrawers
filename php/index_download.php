06b7fda361d3bc0e5c942427c24bae2b85a5217c
joshfiel <joshfiel>
2004-09-15 18:54:12 +0000

    code review changes

--- php/index_download.php
+++ php/index_download.php
@@ -4,28 +4,29 @@
  * All Rights Reserved.  See COPYRIGHT.
  */
 
-require_once('../lib/afs.php');
+require_once( '../lib/afs.php' );
 
-if (!extension_loaded('fileinfo')) {
-    dl('fileinfo.' . PHP_SHLIB_SUFFIX);
+if ( !extension_loaded( 'fileinfo' )) {
+    dl( 'fileinfo.' . PHP_SHLIB_SUFFIX );
 }
 
-if (!extension_loaded('fileinfo')) {
-    die("fileinfo extension is not avaliable, please compile it.\n");
+if ( !extension_loaded( 'fileinfo' )) {
+    error_log( 'mFile: fileinfo extension is not avaliable, please compile it.' );
+    exit();
 }
 
 $download = new Afs();
-$download->setPath($_GET['path']);
-$res = finfo_open(FILEINFO_MIME);
+$download->setPath( $_GET['path'] );
+$res = finfo_open( FILEINFO_MIME );
 
-header("Pragma: public");
-header("Expires: 0");
-header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+header( "Pragma: public" );
+header( "Expires: 0" );
+header( "Cache-Control: must-revalidate, post-check=0, pre-check=0" );
 
-header("Content-Type: " . finfo_file($res, $download->path) );
-header("Content-Disposition: attachment; filename=".basename($download->path) );
+header( "Content-Type: " . finfo_file( $res, $download->path ));
+header( "Content-Disposition: attachment; filename=" . basename( $download->path ));
 
-header("Content-Description: File Transfer");
-@readfile($download->path);
-finfo_close($res);
+header( "Content-Description: File Transfer" );
+@readfile( $download->path );
+finfo_close( $res );
 ?>
