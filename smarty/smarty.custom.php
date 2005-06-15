144cc32141eff4991ef15130c73127b7df9dce49
scam <scam>
2005-06-15 19:54:08 +0000

    modified smarty configuration to use DOCUMENT_ROOT correctly instead of a fixed path for smarty variables.

--- smarty/smarty.custom.php
+++ smarty/smarty.custom.php
@@ -6,10 +6,15 @@ class Smarty_Template extends Smarty {
     function Smarty_Template() {
         $this->Smarty();
 
-        $this->template_dir = '/usr/local/projects/mfile/smarty/templates/';
-        $this->compile_dir  = '/usr/local/projects/mfile/smarty/templates_c/';
-        $this->config_dir   = '/usr/local/projects/mfile/smarty/configs/';
-        $this->cache_dir    = '/usr/local/projects/mfile/smarty/cache/';
+        $this->template_dir = $_SERVER[DOCUMENT_ROOT] .
+                              "../smarty/templates/";
+        $this->compile_dir  = $_SERVER[DOCUMENT_ROOT] .
+                              "../smarty/templates_c/";
+        $this->config_dir   = $_SERVER[DOCUMENT_ROOT] .
+                              "../smarty/configs/";
+        $this->cache_dir    = $_SERVER[DOCUMENT_ROOT] .
+                              "../smarty/cache/";
+
     }
 }
 
