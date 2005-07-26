c57cf7642e6855e6cd409aab2e79daec968077ea
willn <willn>
2005-07-26 20:01:24 +0000

    add noscript html

---
+++ php/scripterror.php
@@ -0,0 +1,9 @@
+<h1>Javascript Error</h1>
+
+<p>
+JavaScript must be enabled in order for you to use this service. However,
+it seems JavaScript is either disabled or not supported by your
+browser. To use our service, enable JavaScript by changing your browser
+options, or use another browser, and then try again.
+</p>
+
