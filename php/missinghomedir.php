22603bb1d1e7d9e0f30355e5ddc1206b22f12a22
fieldsj <fieldsj>
2008-01-11 17:13:46 +0000

    Adding two files that should have been in 0.4.0 release

---
+++ php/missinghomedir.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * Copyright (c) 2008 Regents of The University of Michigan.
+ * All Rights Reserved.  See COPYRIGHT.
+ */
+
+require_once( 'smarty.custom.php' );
+
+$smarty = new Smarty_Template;
+$smarty->assign( 'filedrawers_title', 'missing home directory');
+$smarty->assign( 'redirect', true );
+$smarty->assign( 'stylesheets', $stylesheets);
+$smarty->display( 'missinghomedir.tpl' );
+?>
---
+++ smarty/missinghomedir.tpl
@@ -0,0 +1,11 @@
+{include file="header.tpl"}
+{include file="banner.tpl"}
+
+<div id="error">
+<h1>Missing Home Directory</h1>
+
+<p>
+Either you do not have a home directory set, or it is missing or unreadable.
+Please contact your help desk for assistance in resolving this issue. 
+</p>
+</div>
