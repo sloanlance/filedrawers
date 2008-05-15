cdf688d6522f1e876adce0ffe5f6372d51cddf36
fieldsj <fieldsj>
2008-05-15 15:13:48 +0000

    Cache control fix for file download over SSL for IE

--- php/index_download.php
+++ php/index_download.php
@@ -4,6 +4,11 @@
  * All Rights Reserved.  See COPYRIGHT.
  */
 
+/* IE does not accept cache control headers for downloads over SSL
+ * For more information, see: http://support.microsoft.com/kb/323308
+ * private_no_expire prevents filedrawers from sending the headers
+ */
+session_cache_limiter( 'private_no_expire' );
 require_once( 'afs.php' );
 
 $download = new Afs( $_GET['path'] );
