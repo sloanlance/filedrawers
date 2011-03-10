55a6cf3e7ebeb390e64ed44b212661b2cd3a3944
Kris Steinhoff <steinhof@umich.edu>
2011-03-10 12:07:21 -0500

    added Josh's work for the webapp module

---
+++ application/modules/webapp/controllers/ErrorController.php
@@ -0,0 +1,56 @@
+<?php
+
+class ErrorController extends Zend_Controller_Action
+{
+    public $contexts = array(
+        'error' => array('xml', 'json', 'html')
+    );
+
+
+    public function init()
+    {
+        $this->_context = $this->_helper->getHelper('contextSwitch');
+        $this->_context->setContext('xml', array(
+            'callbacks' => array('post' => array($this->_helper, 'xmlSerialize'))
+        ));
+        $this->_context->initContext();
+    }
+
+
+    public function errorAction()
+    {
+        $errors = $this->_getParam('error_handler');
+        $trace = $errors->exception->getTrace();
+
+        switch ($errors->type) {
+            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ROUTE:
+            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
+            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
+
+                // 404 error -- controller or action not found
+                $this->getResponse()->setHttpResponseCode(404);
+                $this->view->errorMsg = 'Page not found';
+                break;
+            default:
+                // application error
+                error_log($errors->exception->getTraceAsString());
+                $this->getResponse()->setHttpResponseCode(500);
+                $this->view->errorMsg = $errors->exception->getMessage();
+                break;
+        }
+
+        // conditionally display exceptions
+        if ($this->getInvokeArg('displayExceptions') == true) {
+            $this->view->displayExceptions = true;
+            $this->view->exception = $errors->exception;
+            $this->view->request   = $errors->request;
+        }        
+    }
+
+    public function missingAction()
+    {
+        $this->getResponse()->setHttpResponseCode(404);
+        $this->getResponse()->setRawHeader('HTTP/1.1 404 Not Found');
+    }
+}
+
---
+++ application/modules/webapp/controllers/IndexController.php
@@ -0,0 +1,86 @@
+<?php
+/* $Revision: $
+ *
+ * Copyright (c) 2010 Regents of the University of Michigan.
+ * All rights reserved.
+ */
+
+class IndexController extends Zend_Controller_Action
+{
+    protected $_filesystem;
+    protected $_flashMessenger = null;
+    protected $_redirector = null;
+
+    public function init()
+    {
+        $this->_filesystem = Zend_Registry::get('filesystem');
+        $this->_flashMessenger = $this->_helper->getHelper('FlashMessenger');
+        $this->_redirector = $this->_helper->getHelper('Redirector');
+    }
+
+
+    public function preDispatch()
+    {
+        $this->view->messages = $this->_flashMessenger->getMessages();
+    }
+
+    public function listAction()
+    {
+        $path = $this->_request->getUserParam('path');
+
+        if (empty($path)){
+            $path = $this->_filesystem->getHomeDir();
+        }
+
+        $uploadForm = new Form_UploadForm();
+        $uri = $uploadForm->getElement('returnToURI');
+        $sid = $uploadForm->getElement('sessionid');
+        $uri->setValue($this->view->serverUrl() .
+                $this->view->url(array($sid->getHash(), $path),
+                'finishUpload', false, false));
+        $formPath = $uploadForm->getElement('path');
+        $formPath->setValue($path);
+
+        $this->view->uploadForm = $uploadForm;
+
+        $this->view->path  = $path;
+        $this->view->files = $this->_filesystem->listDirectory($path);
+
+        if (!$this->view->files) {
+            $this->view->errorMsg = $this->_filesystem->errorMsg;
+        }
+    }
+
+
+    public function finishAction()
+    {
+        $filters = array(
+            '*' => 'StringTrim'
+        );
+
+        $validators = array(
+            'id' => array()
+        );
+
+        $input = new Zend_Filter_Input($filters, $validators, $_GET);
+        if ( ! $input->isValid()) {
+            $this->_response = array(
+                'errorCode' => 5,
+                'errorMsg' => $input->getMessages()
+            );
+
+            return;
+        }
+
+        $upload = new Model_UploadStatus($input->id);
+        if ( ! $upload->isValid()) {
+            $this->_response = array(
+                'errorCode' => 5,
+                'errorMsg' => $upload->getMessages()
+            );
+        }
+
+        $this->_redirector->gotoRoute(array('path' => $path), 'list', false, false);
+    }
+}
+
