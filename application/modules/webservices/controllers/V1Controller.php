b344d944fef08e378ce360cfd7423f1cefd1c296
Kris Steinhoff <steinhof@umich.edu>
2011-03-24 15:56:31 -0400

    added an easy way to add new versions of the API

---
+++ application/modules/webservices/controllers/V1Controller.php
@@ -0,0 +1,18 @@
+<?php
+/* $Revision: $
+ *
+ * Copyright (c) 2010 Regents of the University of Michigan.
+ * All rights reserved.
+ */
+
+class Webservices_V1Controller extends Webservices_IndexController {
+    /*
+        // This call to init() is only necessary if this version of the API needsr
+        // to override one of its parent's view scripts.
+    public function init()
+    {
+        parent::init();
+        $this->_view->addScriptPath( APPLICATION_PATH .'/modules/webservices/views/scripts/v1/' );
+    }
+     */
+}
