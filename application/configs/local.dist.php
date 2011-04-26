c3ab1bfc8968f83bee1ac402fe17b32593d87006
Kris Steinhoff <steinhof@umich.edu>
2011-04-26 15:06:57 -0400

    updated example config

--- application/configs/local.dist.php
+++ application/configs/local.dist.php
@@ -3,8 +3,13 @@ $conf[ 'phpSettings' ][ 'display_startup_errors' ] = '1';
 $conf[ 'phpSettings' ][ 'display_errors' ] = '1';
 $conf[ 'phpSettings' ][ 'error_reporting' ] = '6143';
 
-$conf[ 'filesystem' ][ 'services' ][ 'active' ][ 'afs' ] = 'AFS';
-$conf[ 'filesystem' ][ 'services' ][ 'active' ][ 'cifs' ] = 'CIFS';
+
+$conf[ 'filesystem' ][ 'services' ][ 'default' ] = 'ifs';
+unset( $conf[ 'filesystem' ][ 'services' ][ 'active' ] );
+$conf[ 'filesystem' ][ 'services' ][ 'active' ][ 'ifs' ] = 'IFS';
+$conf[ 'filesystem' ][ 'services' ][ 'active' ][ 'mainstreamStorage' ] = 'Mainstream Storage';
+
+$conf[ 'resources' ][ 'frontController' ][ 'params' ][ 'displayExceptions' ] = TRUE;
 
 // $conf[ 'resources' ][ 'db' ][ 'params' ][ 'host' ]     = 'db-host';
 // $conf[ 'resources' ][ 'db' ][ 'params' ][ 'username' ] = 'db-user';
