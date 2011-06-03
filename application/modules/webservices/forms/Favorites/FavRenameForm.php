5786896e31767c2504930f35f6901d3ff1635824
Jalpa Patel <jalpa@umich.edu>
2011-06-03 11:59:44 -0400

    initial commit

---
+++ application/modules/webservices/forms/Favorites/FavRenameForm.php
@@ -0,0 +1,59 @@
+<?php
+
+class Form_Favorites_FavRenameForm extends Zend_Form
+{
+    protected $_csrfToken = null;
+    protected $_path = null;
+
+    public function __construct($csrfToken, $path)
+    {
+        $this->_csrfToken = $csrfToken;
+        $this->_path = $path;
+        parent::__construct();
+    }
+
+    public function init()
+    {
+        $this->setMethod('post');
+        $this->setName('rename');
+
+        $oldName = new Zend_Form_Element_Text('oldName');
+        $oldName->setLabel('Old Name');
+        $oldName->addPrefixPath('Filedrawers_Validate', 'Filedrawers/Validate/', 'validate');
+        $oldName->setRequired(true)
+            ->addValidator('NotEmpty', true, array('messages' => array('isEmpty' =>
+                '*You must specifiy the name of the file or folder to rename.')
+            )
+        );
+
+        $oldName->addValidator('FavoritesPath', true, array(
+            //'modifyable' => true,
+    //        'pathContext' => 'path'
+			'oldexists' => false 
+        ));
+
+        $oldName->addFilter('StringTrim');
+        $this->addElement($oldName);
+
+        $newName = new Zend_Form_Element_Text('newName');
+        $newName->addPrefixPath('Filedrawers_Validate', 'Filedrawers/Validate/', 'validate');
+        $newName->setLabel('New Name');
+        $newName->setRequired(true)
+            ->addValidator('NotEmpty', true, array('messages' => array('isEmpty' =>
+                '**You must specifiy a new name for the file or folder.')
+            )
+        );
+
+        $newName->addValidator('FavoritesPath', true, array(
+            //'pathContext' => 'path',
+            'exists' => true 
+        ));
+
+
+        $newName->addFilter('StringTrim');
+        $this->addElement($newName);
+
+        $this->addElement($this->_csrfToken);
+        $this->addElement('submit', 'Submit');
+    }
+}
