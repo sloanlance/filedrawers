016c3c0437f77d6037e002b1f6e0b7845c04a6c9
Jalpa Patel <jalpa@umich.edu>
2011-06-03 14:48:28 -0400

    changed class name to match file name.

---
+++ application/modules/webservices/forms/Favorites/DeleteForm.php
@@ -0,0 +1,38 @@
+<?php
+
+class Form_Favorites_DeleteForm extends Zend_Form
+{
+    protected $_csrfToken = null;
+    protected $_options = array();
+
+    public function __construct($csrfToken, $options)
+    {
+        $this->_options = $options;
+        $this->_csrfToken = $csrfToken;
+        parent::__construct();
+    }
+
+    public function init()
+    {
+        $this->setMethod('post');
+        $this->setName('delete');
+
+        $folderName = new Zend_Form_Element_Text('folderName');
+        $folderName->addPrefixPath('Filedrawers_Validate', 'Filedrawers/Validate/', 'validate');
+        $folderName->setLabel('Delete Fav Name');
+        $folderName->addFilter('StringTrim');
+        $folderName->setRequired(true)
+            ->addValidator('NotEmpty', true, array('messages' => array('isEmpty' =>
+                'You must specifiy a name for the favorite.')
+            )
+        );
+        $folderName->addValidator('FavoritesPath', true, array(
+            //'pathContext' => 'path',
+            'oldexists' => false 
+        ));
+        $this->addElement($folderName);
+
+        $this->addElement($this->_csrfToken);
+        $this->addElement('submit', 'Submit');
+    }
+}
